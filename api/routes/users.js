511d48a8bbfbb291a4cb8f3133f84ebc7b99dfb1
Thomas <tbenhamou@gmail.com>
2020-04-21 00:17:46 +0300

    fix: add checkAuth Middleware for deleting user

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const checkAuth = require('../middlewares/check-auth');
+
 const UserController = require('../controllers/users');
 
 router.post("/register", UserController.register);
 
 router.post("/login", UserController.login);
 
-router.delete("/:userId", UserController.delete);
+router.delete("/:userId", checkAuth, UserController.delete);
 
 module.exports = router;
